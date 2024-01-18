f042e5a458e4753903c3f9e823787d2d6d277763
neerajreddy9989 <naganeerajreddy9989@gmail,com>
2024-01-18 23:16:25 +0530

    main

---
+++ server/src/main/java/com/rozmer/service/controller/SecurityConfig.java
@@ -0,0 +1,26 @@
+// package com.rozmer.service.controller;
+
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//     @Override
+//     protected void configure(HttpSecurity http) throws Exception {
+//         http
+//                .cors() // Enable CORS configuration
+//                 .and()
+//                 .authorizeRequests()
+//                 .antMatchers("/guest/**").permitAll()
+//                 .anyRequest().authenticated()
+//                 .and()
+//                 .formLogin()
+//                 .loginPage("/login")
+//                 .permitAll();
+//     }
+// }
+
