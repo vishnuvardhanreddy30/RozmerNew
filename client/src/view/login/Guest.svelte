4808ae26ba221a7d6abdc0cb834b5db685c188ff
neerajreddy9989 <naganeerajreddy9989@gmail,com>
2024-01-19 15:26:57 +0530

    guest login

--- client/src/view/login/Guest.svelte
+++ client/src/view/login/Guest.svelte
@@ -1,6 +1,7 @@
 <script>
     import { createEventDispatcher } from "svelte";
     import Utils from "../../util/Utils";
+
     import TextField from "../../widget/fields/TextField.svelte";
     import EmailField from "../../widget/fields/EmailField.svelte";
     import CheckBox from "../../widget/fields/CheckBox.svelte";
@@ -11,6 +12,7 @@
     import Request from "../../util/Request";
     import urlConst from "../../const/Url";
     import SessionUtil from "../../util/SessionUtil";
+
     import logo from "../../assets/logo.png";
 
     const dispatch = createEventDispatcher();
@@ -18,15 +20,14 @@
         fullWidth = true,
         name,
         pass,
-        guestEmail,
-        guestPassword,
-        showLoginForm = true;
+        role;
 
     function onSuccess(resp) {
         Utils.log(resp);
 
         if (Utils.isEmpty(resp)) {
             SessionUtil.removeAll();
+
             return onFailure();
         }
 
@@ -41,31 +42,15 @@
     }
 
     const onLogin = () => {
-        if (Utils.isEmpty(name) || Utils.isEmpty(pass)) {
-            return Utils.alert(Labels.required.all_req, Labels.alert.info);
-        }
+        // if (Utils.isEmpty(name) || Utils.isEmpty(pass)) {
+        //     return Utils.alert(Labels.required.all_req, Labels.alert.info);
+        // }
 
         let data = {
             email: name,
             password: pass,
-            ignoreCheck: true
-        };
-
-        Request.post(
-            urlConst.login + Utils.encodeForUrl(data),
-            data,
-            onSuccess,
-            onFailure,
-            onSuccess
-        );
-    }
-
-    function onGuestLogin() {
-        let data = {
-            email: guestEmail,
-            password: "guest",
             ignoreCheck: true,
-            role:"guest"
+            role:role
         };
 
         Request.post(
@@ -75,76 +60,53 @@
             onFailure,
             onSuccess
         );
-    }
+    };
 
     function onRegister() {
+        // dispatch("changeview", "register");
         Utils.redirectTo("register");
     }
 
     function onResetPassword() {
         Utils.redirectTo("reset");
     }
-
-    function toggleForm() {
-        showLoginForm = !showLoginForm;
-    }
 </script>
 
 <div class="login-cont flex-cont wh-100-percent">
     <div class="login-left flex-1">
         <img src={logo} alt="" class="sub-logo" />
+        <!-- <div class="login-title-cont">
+            {Labels.login.flash_msg}
+        </div> -->
     </div>
 
     <div class="login-right flex-1 flex-cont flex-vh bg-standard">
         <div class="margin-bottom-to-child-15 p2 login-items">
             <img src={logo} alt="" class="sub-logo logo-hidden" />
-            <div class="text-center">
-                <Button on:click={toggleForm} text={showLoginForm ? "Guest Login" : "Login"} />
-            </div>
-            {#if showLoginForm}
-                <p class="heading">USER LOG IN</p>
-                <EmailField
-                    label={Labels.login.login_user}
-                    {required}
-                    placeholder="User name or Email*"
-                    bind:value={name}
-                />
-                <PasswordField
-                    label={Labels.login.password}
-                    placeholder="Password*"
-                    {required}
-                    bind:value={pass}
-                    on:enter={onLogin}
-                />
-                <Button
-                text={Labels.login.login}
-                on:click={onLogin}
+            <!-- <TextField
+                label={Labels.login.login_user}
+                {required}
+                bind:value={name}
+            /> -->
+               <TextField
 
-                {fullWidth}
+                {required}
+                bind:value={role}
             />
-            {:else}
-                <!-- Guest Login Section -->
-                <p class="heading">GUEST LOG IN</p>
-
-                <EmailField
-                    label="Guest Email"
-                    placeholder="guest@example.com"
-                    bind:value={guestEmail}
-                />
-                <!-- <PasswordField
-                    label="Guest Password"
-                    placeholder="Guest Password"
-                    bind:value={guestPassword}
-                    on:enter={onGuestLogin}
-                /> -->
-                <Button
-                text="Guest Login"
-                on:click={onGuestLogin}
-
-                {fullWidth}
+            <p class="heading">USER LOG IN</p>
+            <EmailField
+                label={Labels.login.login_user}
+                {required}
+                placeholder="User name or Email*"
+                bind:value={name}
             />
-
-            {/if}
+            <PasswordField
+                label={Labels.login.password}
+                placeholder="Password*"
+                {required}
+                bind:value={pass}
+                on:enter={onLogin}
+            />
             <div class="flex-cont space-between margin-top-20">
                 <CheckBox label={Labels.login.remeber}/>
                 <div class="">
@@ -160,10 +122,15 @@
                     </div>
                 </div>
             </div>
-
+            <Toolbar ui="plain">
+                <Button
+                    text={Labels.login.login}
+                    on:click={onLogin}
+                    slot="center"
+                    {fullWidth}
+                />
+            </Toolbar>
+            <p class="copy-text text-right">Copyright &copy; 2023 Rozmer. All rights reserved.</p>
         </div>
-
-        <p class="copy-text text-right">Copyright &copy; 2023 Rozmer. All rights reserved.</p>
-
     </div>
 </div>
